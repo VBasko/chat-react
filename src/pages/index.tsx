6c55ea703c3d8c0ff53cdc351731edc02af8e649
VBasko <vvbasko@outlook.com>
2024-06-08 12:31:05 -0300

    fix: turn initial page to an entry-point

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
-export const Name = "homepage";
+import authMiddleware from "src/middleware/auth.middleware";
+import { redirect } from "src/router/routes";
 
-export const Page = () => {
-  return <div>Homepage</div>;
+export const Name = "entry-point";
+
+export const Loader = async () => {
+  const middlewareResult = await authMiddleware();
+
+  if (!middlewareResult) {
+    return redirect({ name: "chat" });
+  }
+
+  return middlewareResult;
 };
+
+export const Page = () => null;
