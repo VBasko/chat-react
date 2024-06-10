33ff1fa53ea00c1b809c4921be063116ae5134fd
VBasko <vvbasko@outlook.com>
2024-06-10 18:21:54 -0300

    feat: init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#1c1c17",
+        green: "#97A307",
+        grey: {
+          light: "#C8C7B7",
+          dark: "#47483B",
+        },
+        light: "#e5e2da",
+        red: {
+          light: "#FF5449",
+          dark: "#BA1A1A",
+        },
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Helvetica, Helvetica Fallback", ...defaultTheme.fontFamily.sans],
+    },
+  },
+  plugins: [],
+};
