5e4a2cb83427ec662d430096f01e231e4331deb3
VBasko <vvbasko@outlook.com>
2024-06-24 14:29:46 -0300

    fix: signup.model error messages

--- src/api/models/signup.model.ts
+++ src/api/models/signup.model.ts
@@ -1,21 +1,25 @@
-import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator";
+import { IsEmail, IsStrongPassword } from "class-validator";
 import { IsEqualTo } from "src/api/is-equal-to.validator";
+import { generatePasswordErrorMessage } from "src/api/password.validator";
 
 export class SignupModel {
-  @IsEmail()
+  @IsEmail({}, { message: "Please enter valid email address." })
   email: string;
 
-  @IsNotEmpty()
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message: generatePasswordErrorMessage,
+    }
+  )
   password: string;
 
-  @IsNotEmpty()
-  @IsEqualTo("password")
+  @IsEqualTo("password", { message: "Passwords do not match." })
   confirmPassword: string;
 }
