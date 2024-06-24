6da9d30038416af02e47ac4a98acdc8a7ee16fcd
VBasko <vvbasko@outlook.com>
2024-06-24 14:35:48 -0300

    feat: refactror header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { resolveRoute, useNavigate } from "src/router/routes";
 import { AuthService } from "src/api/services/auth.service";
+import Button from "../button";
 
 const authService = new AuthService();
 
@@ -20,9 +21,9 @@ const Header = () => {
   };
 
   return (
-    <header>
+    <header className="flex items-center justify-between">
       <nav>
-        <ul>
+        <ul className="flex items-center gap-4">
           {links.map((item, index) => {
             return (
               <li key={`nav-link-${index}`}>
@@ -33,7 +34,11 @@ const Header = () => {
         </ul>
       </nav>
 
-      <button onClick={handleLogout}>Logout</button>
+      <div className="flex gap-4">
+        <span>User: {authService.auth.currentUser?.displayName}</span>
+
+        <Button onClick={handleLogout}>Logout</Button>
+      </div>
     </header>
   );
 };
