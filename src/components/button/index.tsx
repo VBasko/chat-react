12bffbfa37f827dccb3ad42c2c9d70c83722db60
VBasko <vvbasko@outlook.com>
2024-06-24 14:37:07 -0300

    feat: Button component add onClick prop function

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,12 +5,14 @@ type ButtonTypes = "submit" | "reset" | "button";
 interface ButtonProps {
   children: React.ReactNode;
   type?: ButtonTypes;
+  onClick?: () => void;
 }
 
-const Button = ({ children, type = "button" }: ButtonProps) => {
+const Button = ({ children, type = "button", onClick }: ButtonProps) => {
   return (
     <button
       type={type}
+      onClick={onClick}
       className="flex gap-x-2 px-6 py-2.5 bg-primary text-on-primary rounded-full relative overflow-hidden before:absolute before:inset-0 before:size-full before:bg-on-primary/[8%] before:opacity-0 before:rounded-[inherit] hover:before:opacity-100 focus:before:bg-on-primary/[12%] transition-all duration-500"
     >
       {children}
