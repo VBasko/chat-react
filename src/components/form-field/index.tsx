88b5281e6d7fe88b66c57f820fe761d358d7c32b
VBasko <vvbasko@outlook.com>
2024-07-01 15:19:45 -0300

    style: FormField component

--- src/components/form-field/index.tsx
+++ src/components/form-field/index.tsx
@@ -7,25 +7,25 @@ interface FormFieldProps {
   message?: string;
 }
 
-export const FormField = (props: FormFieldProps) => {
+const FormField = (props: FormFieldProps) => {
   return (
-    <>
-      <div className="">
-        {props.label && (
-          <label htmlFor={`${props.name.toLowerCase()}`}>{props.label}</label>
-        )}
-        <input
-          id={`${props.name}`}
-          type={props.type}
-          placeholder={props.placeholder}
-          name={props.name.toLowerCase()}
-          className="block w-full px-4 py-2 bg-black border-2 border-grey-light rounded-md text-sm placeholder-grey-light focus:outline-none focus:border-green focus:ring-1 focus:ring-green disabled:bg-grey-dark disabled:border-grey-dark invalid:border-red-light invalid:text-red-light
+    <div>
+      {props.label && (
+        <label htmlFor={`${props.name.toLowerCase()}`}>{props.label}</label>
+      )}
+      <input
+        id={`${props.name}`}
+        type={props.type}
+        placeholder={props.placeholder}
+        name={props.name.toLowerCase()}
+        className="block w-full mb-1 px-4 py-2 bg-black border-2 border-grey-light rounded-md text-sm placeholder-grey-light focus:outline-none focus:border-green focus:ring-1 focus:ring-green disabled:bg-grey-dark disabled:border-grey-dark invalid:border-red-light invalid:text-red-light
       focus:invalid:border-red-light focus:invalid:ring-red-light"
-        />
-      </div>
+      />
       {props.error && (
         <span className="text-sm text-red-light">{props.message}</span>
       )}
-    </>
+    </div>
   );
 };
+
+export default FormField;
