baab5418d1bb53af39274e2bb233945a13f7f74d
VBasko <vvbasko@outlook.com>
2024-07-01 15:19:09 -0300

    feat: add className inheritance to Button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,19 +1,29 @@
 import React from "react";
+import clsx from "clsx";
 
 type ButtonTypes = "submit" | "reset" | "button";
 
 interface ButtonProps {
   children: React.ReactNode;
   type?: ButtonTypes;
+  className?: string;
   onClick?: () => void;
 }
 
-const Button = ({ children, type = "button", onClick }: ButtonProps) => {
+const Button = ({
+  children,
+  type = "button",
+  className,
+  onClick,
+}: ButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="flex gap-x-2 px-6 py-2.5 bg-primary text-on-primary rounded-full relative overflow-hidden before:absolute before:inset-0 before:size-full before:bg-on-primary/[8%] before:opacity-0 before:rounded-[inherit] hover:before:opacity-100 focus:before:bg-on-primary/[12%] transition-all duration-500"
+      className={clsx(
+        "flex gap-x-2 px-6 py-2.5 bg-primary text-on-primary rounded-full relative overflow-hidden before:absolute before:inset-0 before:size-full before:bg-on-primary/[8%] before:opacity-0 before:rounded-[inherit] hover:before:opacity-100 focus:before:bg-on-primary/[12%] transition-all duration-500",
+        className
+      )}
     >
       {children}
     </button>
