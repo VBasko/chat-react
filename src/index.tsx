686073286158da7822d975f648ad5f08adede0cb
VBasko <vvbasko@outlook.com>
2024-07-02 00:44:32 -0300

    feat: wrap app in redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./router";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <Provider store={store}>
+      <RouterProvider router={Router} />
+    </Provider>
   </React.StrictMode>
 );
